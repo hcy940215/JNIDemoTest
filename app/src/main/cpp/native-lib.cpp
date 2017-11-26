41192103d436c3d4b9cf5d566877bc2c4474cad0
hcy940215 <894442036@qq.com>
2017-11-26 20:07:03 +0800

    JNI基础 访问java方法 数组 异常 引用

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,15 @@
 #include <jni.h>
 #include <string>
 #include <string.h>
+#include <android/log.h>
+#include "wchar.h"
 
-
+#define TAG "TAG"
+#define LOGI(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_shengmingji_jnidemotest_MainActivity_accessStaticMethod(JNIEnv *env, jobject instance) {
+Java_com_shengmingji_jnidemotest_MainActivity_accessStaticMethod
+        (JNIEnv *env, jobject instance) {
 
     jclass jclz = env->GetObjectClass(instance);
     jmethodID jmid = env->GetStaticMethodID(jclz, "getRandomUUID", "()Ljava/lang/String;");
@@ -46,7 +50,8 @@ Java_com_shengmingji_jnidemotest_MainActivity_stringFromJNI(
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_shengmingji_jnidemotest_MainActivity_get(JNIEnv *env, jobject instance) {
+Java_com_shengmingji_jnidemotest_MainActivity_get
+        (JNIEnv *env, jobject instance) {
     printf("invoke set in c++\n");
 
 
@@ -55,7 +60,8 @@ Java_com_shengmingji_jnidemotest_MainActivity_get(JNIEnv *env, jobject instance)
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_shengmingji_jnidemotest_MainActivity_set(JNIEnv *env, jobject instance, jstring str_) {
+Java_com_shengmingji_jnidemotest_MainActivity_set
+        (JNIEnv *env, jobject instance, jstring str_) {
     const char *str = env->GetStringUTFChars(str_, 0);
     printf("invoke set in c++\n");
     env->ReleaseStringUTFChars(str_, str);
@@ -66,7 +72,8 @@ Java_com_shengmingji_jnidemotest_MainActivity_set(JNIEnv *env, jobject instance,
  */
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_shengmingji_jnidemotest_MainActivity_print(JNIEnv *env, jclass jclaz) {
+Java_com_shengmingji_jnidemotest_MainActivity_print
+        (JNIEnv *env, jclass jclaz) {
     return env->NewStringUTF("hahahaha");
 }
 
@@ -76,7 +83,8 @@ Java_com_shengmingji_jnidemotest_MainActivity_print(JNIEnv *env, jclass jclaz) {
  */
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_shengmingji_jnidemotest_MainActivity_accessField(JNIEnv *env, jobject instance) {
+Java_com_shengmingji_jnidemotest_MainActivity_accessField
+        (JNIEnv *env, jobject instance) {
 
     jclass jclz = env->GetObjectClass(instance);
     jfieldID jfield = env->GetFieldID(jclz, "key", "Ljava/lang/String;");
@@ -97,7 +105,8 @@ Java_com_shengmingji_jnidemotest_MainActivity_accessField(JNIEnv *env, jobject i
  */
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_shengmingji_jnidemotest_MainActivity_accessStaticField(JNIEnv *env, jobject instance) {
+Java_com_shengmingji_jnidemotest_MainActivity_accessStaticField
+        (JNIEnv *env, jobject instance) {
     jclass jcz = env->GetObjectClass(instance);
 
     jfieldID jfield = env->GetStaticFieldID(jcz, "count", "I");
@@ -108,3 +117,189 @@ Java_com_shengmingji_jnidemotest_MainActivity_accessStaticField(JNIEnv *env, job
 
 }
 
+/**
+ * 访问构造方法
+ */
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_accessConstructor
+        (JNIEnv *env, jobject instance) {
+
+    //通过路径找到对应的类
+    jclass jclz = env->FindClass("java/util/Date");
+    //构造方法名 都用<init>
+    jmethodID jmId = env->GetMethodID(jclz, "<init>", "()V");
+
+    //实例化 Date对象 jni引用都返回jobject
+    jobject jobjDate = env->NewObject(jclz, jmId);
+
+    jmethodID time_mid = env->GetMethodID(jclz, "getTime", "()J");
+
+    jlong time = env->CallLongMethod(jobjDate, time_mid);
+
+    return time;
+
+}
+
+/**
+ *  访问字符串
+ */
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_chineseChar
+        (JNIEnv *env, jobject instance, jstring jstr) {
+
+    char *c_str = (char *) env->GetStringUTFChars(jstr, NULL);
+    // LOGI(c_str, "%s", c_str);
+    return env->NewStringUTF(c_str);
+}
+
+/**
+ * 访问基本类型数组
+ */
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_accessArray
+        (JNIEnv *env, jobject instance, jintArray arr_) {
+
+    //对java层数据转化
+    jint *arr = env->GetIntArrayElements(arr_, NULL);
+
+    if (arr == NULL) {
+        return;
+    }
+
+    //数组长度
+    int len = env->GetArrayLength(arr_);
+//    for (int i = 0; i < len; i++) {
+//        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "%d", arr[i]);
+//    }
+
+    env->ReleaseIntArrayElements(arr_, arr, 0);
+}
+
+/**
+ * 访问引用型数组
+ */
+extern "C"
+JNIEXPORT jobjectArray JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_accessStringArray
+        (JNIEnv *env, jobject instance, jint size) {
+
+    jclass jclaz = env->FindClass("java/lang/String");
+    if (jclaz == NULL) {
+        return NULL;
+    }
+
+    //创建数组 VS中NewObjectArray(size, jclaz, instance);
+    jobjectArray jobjArr = env->NewObjectArray(size, jclaz, NULL);
+    if (jobjArr == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < size; i++) {
+        //c 字符串
+        char *c_str = new char[256];
+        memset(c_str, 0, 256);
+
+        sprintf(c_str, "hello %d", i);
+        //c->String
+        jstring str = env->NewStringUTF(c_str);
+        if (str == NULL) {
+            return NULL;
+        }
+
+        //将jstring复制给数组
+        env->SetObjectArrayElement(jobjArr, i, str);
+        delete (c_str);
+    }
+
+    return jobjArr;
+}
+
+/**
+ * 局部引用 不能多进程
+ */
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_localRef
+        (JNIEnv *env, jobject instance) {
+
+
+    for (int i = 0; i < 5; ++i) {
+        jclass jclaz = env->FindClass("java/util/Date");
+        jmethodID jmid = env->GetMethodID(jclaz, "<init>", "()V");
+
+        //创建一个局部引用 方式FindClass  NewObject GetObjectClass
+        jobject jobj = env->NewObject(jclaz, jmid);
+
+        //释放引用
+        //方式 JVM自动释放
+        //②手动释放
+        env->DeleteLocalRef(jobj);
+    }
+
+}
+
+/**
+ * 全局引用 能多进程 跨方法使用
+ * NewGlobalRef唯一方式
+ */
+
+jstring str;
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_globalRef
+        (JNIEnv *env, jobject instance) {
+    jobject obj = env->NewStringUTF("JNI global Ref");
+    str = (jstring) env->NewGlobalRef(obj);
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_getGlobalRef
+        (JNIEnv *env, jobject instance) {
+    return str;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_delGlobalRef
+        (JNIEnv *env, jobject instance) {
+    env->DeleteGlobalRef(str);
+    __android_log_print(ANDROID_LOG_VERBOSE, TAG, "释放");
+}
+
+//弱全局引用
+jstring g_week_clas;
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_weakGlobalRef
+        (JNIEnv *env, jobject instance) {
+
+    jobject obj = env->NewStringUTF("JNI WeakGlobalRef Ref");
+    g_week_clas = (jstring) env->NewWeakGlobalRef(obj);
+    return g_week_clas;
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_shengmingji_jnidemotest_MainActivity_exception
+        (JNIEnv *env, jobject instance) {
+
+    jclass jclaz = env->GetObjectClass(instance);
+    jfieldID jfid = env->GetFieldID(jclaz, "ke", "Ljava/lang/String;");
+
+    jthrowable ex = env->ExceptionOccurred();
+    if (ex != NULL) {
+        env->ExceptionClear();
+        jclass newExc = env->FindClass("java/lang/IllegalArgumentException");
+
+        if (newExc==NULL){
+            __android_log_print(ANDROID_LOG_VERBOSE,TAG,"jni exception");
+            return NULL;
+        }
+        env->ThrowNew(newExc,"Throw exception from JNI: GetFieldID ");
+    }
+    return NULL;
+}
--- app/src/main/java/com/shengmingji/jnidemotest/MainActivity.java
+++ app/src/main/java/com/shengmingji/jnidemotest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.shengmingji.jnidemotest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -12,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     String key = "key";
     static int count = 9;
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,15 +41,39 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("TAG", "accessMethod: " + accessMethod());
 
-        Log.i("TAG", "accessStaticMethod: "+accessStaticMethod());
+        Log.i("TAG", "accessStaticMethod: " + accessStaticMethod());
+
+        Log.i("TAG", "accessConstructor: " + accessConstructor());
+
+        Log.i("TAG", "chineseChar: " + chineseChar("哈啊哈"));
+        int nums[] = {10, 15, 2, 6, 8, 8, 5, 20};
+
+        accessArray(nums);
+        Log.i("TAG", "accessArray: ");
+
+        String[] stringArray = accessStringArray(10);
+        for (int i = 0; i < stringArray.length; i++) {
+            Log.i("TAG", "accessStringArray: >>>  " + stringArray[i]);
+        }
+
+        globalRef();
+        String globalRef = getGlobalRef();
+        Log.i("TAG", "getGlobalRef: " + globalRef);
+        delGlobalRef();
+
+        Log.i("TAG", "weakGlobalRef: " + weakGlobalRef());
+
+        try {
+
+            exception();
+        }catch (Exception e){
+            Log.i("TAG", "exception: "+e.getMessage());
+        }
+
+        Log.i("TAG", "exception: --------------------");
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     public native String stringFromJNI();
-
 
     public native String accessField();
 
@@ -55,15 +83,35 @@ public class MainActivity extends AppCompatActivity {
 
     public native String accessStaticMethod();
 
+    public native long accessConstructor();
+
+    public native String chineseChar(String str);
+
+    public native void accessArray(int[] arr);
+
+    public native String[] accessStringArray(int arr);
+
+    public native void localRef();
+
+    public native void globalRef();
+
+    public native String getGlobalRef();
+
+    public native void delGlobalRef();
+
+    public native String weakGlobalRef();
+
+    public native String exception();
+
     public native void set(String str);
 
     public static native String print();
 
-    int getRandom(int max){
+    int getRandom(int max) {
         return new Random().nextInt(max);
     }
 
-    static String getRandomUUID(){
+    static String getRandomUUID() {
         return UUID.randomUUID().toString();
     }
 }
